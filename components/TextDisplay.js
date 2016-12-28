ea8887f3e5418c770fc190430d8804fa0ef24e9e
abdulmuhaimin-work <abdulmuhaimin.work@gmail.com>
2016-12-28 11:12:58 +0800

    add delete letter button

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class TextDisplay extends Component {
 
+  handleClick() {
+    this.props.deleteLetter()
+  }
+
   render() {
-    return <div>text display here: {this.props.text}</div>
+    return (
+      <div>
+        <div>text display here: {this.props.text}</div>
+        <button onClick={this.handleClick.bind(this)}>delete one letter</button>
+      </div>
+    )
   }
 }
 
