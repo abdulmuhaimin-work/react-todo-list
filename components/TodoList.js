c293200fce3f6ef01b33800cc30b481fd5ffd633
abdulmuhaimin-work <abdulmuhaimin.work@gmail.com>
2016-12-31 19:28:20 +0800

    refactor addtodo, delete and completed

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem'
 
 class TodoList extends Component {
   render() {
     return (
       <ul>
-        {
-          this.props.todos.map((todo) => {
-            return <li key={todo.id}>{todo.text}</li> 
-          })
-        }
+      {
+        this.props.todos.map((todo) => {
+          return <TodoItem key={todo.id} todo={todo} actions={this.props.actions}/>
+        })
+      }
       </ul>
     )
   }
